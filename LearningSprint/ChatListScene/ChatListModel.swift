e558939e6882b496ec923c5fc535fcfb65a2f4d4
MinwooJe <gpatration77@gmail.com>
2024-10-03 23:05:22 +0900

    feat: ChatListModel 구현

---
+++ LearningSprint/ChatListScene/ChatListModel.swift
@@ -0,0 +1,30 @@
+//
+//  ChatListModel.swift
+//  LearningSprint
+//
+//  Created by 제민우 on 10/3/24.
+//
+
+import UIKit
+
+struct ChatListModel {
+    let profileImage: UIImage?
+    let userName: String
+    let address: String
+    let time: String
+    let chatPreviewText: String
+    
+    init(
+        profileImage: UIImage? = UIImage(),
+        userName: String,
+        address: String = "대연동",
+        time: String,
+        chatPreviewText: String
+    ) {
+        self.profileImage = profileImage
+        self.userName = userName
+        self.address = address
+        self.time = time
+        self.chatPreviewText = chatPreviewText
+    }
+}
